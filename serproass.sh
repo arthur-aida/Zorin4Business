65622f9766556c385553331cc5d596d157bab890
Arthur Mitsuharu Aida <99360965+arthur-aida@users.noreply.github.com>
2025-07-28 11:44:21 -0300

    Update serproass.sh
    Atualização

--- serproass.sh
+++ serproass.sh
@@ -21,7 +21,10 @@
 #
 # Os links dos arquivos citados neste script podem ficar desatualizados. EM CASO DE ERROS PESQUISE O NOVO LINK E ATUALIZE NO SCRIPT.
 #echo "Chamada do script: "$(basename $0) "-----------------------------------------------------------------------------------------------------------"
-
+if [ "$(id -u)" != "0" ] ; then
+	zenity   --warning --text="Solicite ao Administrador que execute, como root, executar no terninal (bash serproass.sh) para instal�-lo " --width=650 --height=150 --timeout=15
+	exit
+fi
 nome="assinador-serpro"
 pacote=$(dpkg --get-selections | grep "$nome" )
 if [ ! -n "$pacote" ]; then
@@ -33,10 +36,6 @@ if [ ! -n "$pacote" ]; then
 		apt --fix-broken install -y 
 		rm -f  /usr/share/applications/InstaladorAssinadorSepro.desktop
 	fi
-	if [ "$(id -u)" != "0" ]; then
-		zenity   --warning --text="Solicite ao Administrador que execute, como root, num terminal <bash serproass.sh> para instalá-lo." --width=450 --height=100 --timeout=15
-		exit
-	fi
 else
-		zenity   --warning --text="Na barra de localização do menu, digite: Assinador Serpro " --width=450 --height=100 --timeout=15
+		zenity   --warning --text="Na barra de localiza��o do menu, digite: Assinador Serpro " --width=450 --height=100 --timeout=15
 fi
